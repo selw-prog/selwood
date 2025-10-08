41b63fc692bce1cc903dd3674047a1ddfd56dca0
Sean Elwood <seanrelwood@gmail.com>
2025-10-08 15:21:56 -0500

    var defs, output file

---
+++ terraform/modules/vm_stack/variables.tf
@@ -0,0 +1,43 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "Central US"
+}
+
+variable "name" {
+  description = "Base name for resources"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Size of the virtual machine"
+  type        = string
+  default     = "Standard_B1ls"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
+
+variable "admin_ssh_public_key" {
+  description = "SSH public key for VM access"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_address_prefixes" {
+  description = "Address prefixes for the subnet"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
