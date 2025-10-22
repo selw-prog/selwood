245706fe8fda1a3ef890cc7a6aefc760aea73db4
Sean Elwood <seanrelwood@gmail.com>
2025-10-22 12:44:34 -0500

    public ip, priv key var

--- terraform/archive/vm_stack/main.tf
+++ terraform/archive/vm_stack/main.tf
@@ -18,7 +18,7 @@ module "nsg" {
   nsg_name = "home-nsg"
   location = "Central US"
   resource_group_name = var.resource_group_name
-  ssh_source_ip = "145.223.7.49/32"
+  ssh_source_ip = "145.223.7.47/32"
 }
 
 module "vm_stack" {
@@ -29,5 +29,6 @@ module "vm_stack" {
   vm_size = "Standard_B1ls"
   admin_username = "sean"
   admin_ssh_public_key = file("~/.ssh/id_rsa.pub")
+  admin_ssh_private_key = file("~/.ssh/id_rsa")
   nsg_id = module.nsg.nsg_id
 }
