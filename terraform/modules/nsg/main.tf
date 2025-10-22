95f855ddbe23382f1584e709e874c5298dcbb946
Sean Elwood <seanrelwood@gmail.com>
2025-10-22 12:43:20 -0500

    allow http and https

--- terraform/modules/nsg/main.tf
+++ terraform/modules/nsg/main.tf
@@ -18,4 +18,26 @@ resource "azurerm_network_security_group" "this" {
         source_address_prefix = var.ssh_source_ip
         destination_address_prefix = "*"
     }
+    security_rule {
+        name = "AllowHTTP"
+        priority = 110
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "80"
+        source_address_prefix = var.ssh_source_ip
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name = "AllowHTTPS"
+        priority = 111
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "443"
+        source_address_prefix = var.ssh_source_ip
+        destination_address_prefix = "*"
+    }
 }
